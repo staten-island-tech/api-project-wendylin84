4626ffc30c7ab56fb6317a2d1af19a5965d591c0
wendylin84 <wendyl84@nycstudents.net>
2025-12-12 13:56:46 -0500

    n

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json(); //makes the data into JSON object we can use
+      console.log(data);
+      document.getElementById("api-response").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
