dae5aa65d89eee806c2f0a0b47077f796f73b6e1
wendylin84 <wendyl84@nycstudents.net>
2025-12-22 14:01:53 -0500

    f

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,15 +1,16 @@
 import "./style.css";
 
-const URL = "https://taylor-swift-api.vercel.app/api/albums/Taylor%20Swift";
 async function getData(URL) {
   try {
-    const response = await fetch(URL);
+    const response = await fetch(
+      "https://taylor-swift-api.vercel.app/api/albums/"
+    );
     if (response.status != 200) {
       throw new Error(response);
     } else {
       const data = await response.json(); //makes the data into JSON object we can use
-      console.log(data);
-      document.getElementById("api-response").textContent = data.name;
+      data.forEach((card) => console.log(card));
+      document.getElementById("api-response").textContent = data.title;
     }
   } catch (error) {
     console.log(error);
@@ -17,3 +18,53 @@ async function getData(URL) {
   }
 }
 getData(URL);
+
+getData().then((data) => {
+  console.log(data);
+
+  const ol = document.createElement("ol");
+
+  data.forEach((user) => {
+    const li = document.createElement("li");
+    li.innerHTML = user.name;
+
+    li.style.fontSize = "22px";
+
+    const ul = document.createElement("ul");
+
+    const company = document.createElement("li");
+    company.innerHTML = `Company: ${user.company.name}`;
+
+    const city = document.createElement("li");
+    city.innerHTML = `City: ${user.address.city}`;
+
+    const email = document.createElement("li");
+    email.innerHTML = `Email: ${user.email}`;
+
+    const phone = document.createElement("li");
+    phone.innerHTML = `Phone: ${user.phone}`;
+
+    ul.append(...[company, city, email, phone]);
+
+    li.appendChild(ul);
+
+    ol.appendChild(li);
+  });
+
+  const container = document.getElementById("container");
+  container.appendChild(ol);
+});
+
+/* 
+function inject(card) {
+  const container = document.querySelector("#api-response");
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card">
+    <h2 class="card-title">${card.title}</h2>
+      <img class="card-img" src="${card.img}"/>
+    </div>`
+  );
+}
+data.forEach((card) => inject(card));
+ */
