4c41e6c8511153eefb53849b94642c756d502620
wendylin84 <wendyl84@nycstudents.net>
2026-01-05 14:01:55 -0500

    z

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -20,6 +20,37 @@ getData(URL);
 
 getData().then((data) => {
   console.log(data);
+  const container = document.querySelector("#api-response");
+  data.forEach((card) => {
+    container.insertAdjacentHTML(
+      "afterbegin",
+      `<ul id="list"><li>
+      <div class="card">
+      <h1 class="card-title">${card.title}</h1>
+    <img class="card-album-cover" src="${card.albumCover}" alt="Album Cover">
+    <p class="card-artist">Artist: ${card.artist}</p>
+    <p class="card-release-date">Release Date: ${card.releaseDate}</p>
+    </div>
+    </li>
+    </ul>`
+    );
+  });
+});
+
+function searchAlbum() {
+  let input = document.getElementById("searchbar").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("card-title");
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "list-item";
+    }
+  }
+}
+/* getData().then((data) => {
+  console.log(data);
   let ol = "<ol>";
   data.forEach((card) => {
     ol += ` <li style="font-size: 22px;">${card.title}
@@ -33,10 +64,9 @@ getData().then((data) => {
   ol += "</ol>";
   const container = document.getElementById("container");
   container.innerHTML = ol;
-});
+}); */
 
-/* 
-function inject(card) {
+/* function inject(card) {
   const container = document.querySelector("#api-response");
   container.insertAdjacentHTML(
     "afterbegin",
@@ -46,5 +76,5 @@ function inject(card) {
     </div>`
   );
 }
-data.forEach((card) => inject(card));
+URL.forEach((card) => inject(card));
  */
